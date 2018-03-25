3b80f89af602f876140ed6c1f43c3f4e49527e1d
Slex Atroukov <>
2018-03-25 08:24:23 +0100

    updated worker to pass action when syncing. had to remove debounce as it was filtering out actions

--- src/slexStoreWorker.js
+++ src/slexStoreWorker.js
@@ -2,19 +2,38 @@ import slexStore from 'slex-store'
 import _ from 'lodash'
 
 class SlexWorkerStoreModule {
-  createSyncAction = ({ nextState }) => {
-    return {
-      type: 'SYNC_WITH_WORKER_STORE',
-      nextState
+  createWorkerResponseMiddleware = middleware => {
+    return ({ prevState, nextState, action }) => {
+      if (action.type === 'SYNC_WITH_WORKER_STORE') {
+        const forwardedAction = action.action
+        if (forwardedAction) {
+          return middleware({ prevState, nextState, action: forwardedAction })
+        } else {
+          return middleware({ prevState, nextState, action })
+        }
+      } else {
+        return middleware({ prevState, nextState, action })
+      }
     }
   }
-  createClientReducer = () => {
+  createSyncAction = ({ action, nextState }) => {
+    return {
+      type: 'SYNC_WITH_WORKER_STORE',
+      nextState,
+      action
+    }
+  }
+  createClientReducer = (reducers) => {
+    const baseReducer = slexStore.createReducer(reducers)
     return (state, action) => {
       switch (action.type) {
         case 'SYNC_WITH_WORKER_STORE':
-          return action.nextState
+          return {
+            ...state,
+            ...action.nextState
+          }
         default:
-          return state
+          return baseReducer(state, action)
       }
     }
   }
@@ -34,10 +53,12 @@ class SlexWorkerStoreModule {
     })
     const wrappedApplyDispatch = ({ dispatch, getState, setState, notifyListeners }) => {
       const appliedDispatch = createdDispatch.applyDispatch({ dispatch, getState, setState, notifyListeners })
-      // dispatch sync action to replace store with one received from worker
+      // receive forwarded action from worker
       worker.onmessage = event => {
         if (event.data.type === 'SYNC_WITH_WORKER_STORE') {
-          dispatch(event.data)
+          const action = event.data
+          const { action: originalAction, nextState } = action
+          dispatch(action)
         }
       }
       return appliedDispatch
@@ -48,27 +69,19 @@ class SlexWorkerStoreModule {
     }
     return createdDispatch
   }
-  _getPostMessage = ({ workerGlobalContext, debounce }) => {
-    if (_.isNumber(debounce)) {
-      return _.debounce(workerGlobalContext.postMessage, debounce)
-    } else {
-      return workerGlobalContext.postMessage
-    }
-  }
-  createWorkerDispatch = ({ workerGlobalContext, reducer, middleware = [], sideEffects = [], debounce }) => {
+  createWorkerDispatch = ({ workerGlobalContext, reducer, middleware = [], sideEffects = [] }) => {
     const createdDispatch = slexStore.createDispatch({
       reducer,
       middleware,
       sideEffects
     })
-    const postMessage = this._getPostMessage({ workerGlobalContext, debounce })
     const wrappedApplyDispatch = ({ dispatch, getState, setState, notifyListeners }) => {
       const appliedDispatch = createdDispatch.applyDispatch({ dispatch, getState, setState, notifyListeners })
       const wrappedAppliedDispatch = action => {
         const appliedResult = appliedDispatch(action)
         if (appliedResult.stateChanged) {
           // notify client of new state
-          postMessage(this.createSyncAction({ nextState: appliedResult.nextState }))
+          workerGlobalContext.postMessage(this.createSyncAction({ action, nextState: appliedResult.nextState }))
         }
         return appliedResult
       }
